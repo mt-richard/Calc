538491ee912cc17c2726c1aba252f318c9dd629d
unknown <Richard@festimoltd.com>
2022-08-04 15:58:25 +0200

    my second commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -15,9 +15,24 @@ const Content = () => {
                 const backspace = () => {
                     setResult(result.slice(0, -1))
                 }
+                const squareroot = () => {
+                    setResult(Math.sqrt(result).toString());
+                }
+                const squared = () => {
+                    setResult(Math.pow(result, 2).toString());
+                }
+                const exponent = () => {
+                    setResult(eval(result ^ result).toString());
+                }
+                const oneover = () => {
+                    setResult(eval(1 / result).toString());
+                }
+                const percentage = () => {
+                    setResult(eval(result / 100).toString());
+                }
                 const calculate = () => {
                     try {
-                        setResult(eval(result.toString()));
+                     setResult(eval(result).toString());
                     } catch (error) {
                         setResult("Error");
                     }
@@ -38,17 +53,17 @@ const Content = () => {
                 <div className="display">
                     <form action="" method="post">
                     <input type="text" id="input" name="input" value={result}/><br/>
-                    <input type="text" name="result"/>
+                    <div className="answer text-green-600"> 9999 </div>
                     </form>
                 </div>
             <div className="btns">
                 <div className="upperbtn">
-                    <button onClick={myadd} name="Exp" className="upbtn btnexp">Exp</button>
-                    <button onClick={myadd} name="sqrt" className="upbtn btnsqrt">Sqrt</button>
+                    <button onClick={exponent} name="Exp" className="upbtn btnexp">x<sup>y</sup></button>
+                    <button onClick={squareroot} name="sqrt" className="upbtn btnsqrt">√x</button>
                     <button onClick={backspace} name="clear" className="upbtn btnEx">C</button>
                     <button onClick={clean} name="clearall" className="btn btn-dangerous upbtn btnC">CL</button>
-                    <button onClick={myadd} name="" className="upbtn  btnx">1/x</button>
-                    <button onClick={myadd} name="" className="upbtn btnxp">x^2</button>
+                    <button onClick={oneover} name="" className="upbtn  btnx">1/x</button>
+                    <button onClick={squared} name="" className="upbtn btnxp">x<sup>2</sup></button>
                     <button onClick={myadd} name="/" className="upbtn btnbutton">/</button>
                     <button onClick={myadd} name="*" className="upbtn btnmult">*</button>
                 </div>
@@ -64,11 +79,14 @@ const Content = () => {
                     <button onClick={myadd} name="1" className="lowbtn btn1">1</button>
                     <button onClick={myadd} name="2" className="lowbtn btn2">2</button>
                     <button onClick={myadd} name="3" className="lowbtn btn3">3</button>
-                    <button onClick={myadd} name="()" className="upbtn btn()">()</button>
+                    <button onClick={myadd} name="(" className="upbtn btn()">(</button>
+                   {/*  <button onClick={myadd} name=")" className="upbtn btn()">)</button> */}
                     <button onClick={myadd} name="." className="lowbtn btndot">.</button>
                     <button onClick={myadd} name="0" className="lowbtn btn0">0</button>
-                    <button onClick={myadd} name="%" className="lowbtn btnmod">%</button>
-                    <button onClick={calculate} name="equal" className="bg-green-600 lowbtn btneq">=</button>
+                    <button onClick={percentage} name="%" className="lowbtn btnmod">%</button>
+                    <button onClick={myadd} name=")" className="upbtn btn()">)</button>
+                    <button onClick={myadd} name="Richard" className="ownner btn()">Owner</button>
+                    <button onClick={calculate} name="equal" className="bg-green  btneq">=</button>
                 </div>
             </div>
             </div>
